fed75bbc753730abddd2bfae79194b2060877fd9
VictorCoder1 <enomaedomwonyi@gmail.com>
2023-08-27 19:51:24 +0100

    vercel analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { DefaultSeo } from "next-seo";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="overflow-hidden">
       <GoogleAnalytics trackPageViews />
+      <Analytics />
       <DefaultSeo
         title="TITTA Agency"
         description="We are committed to offering professional services with current technologies backed by years of experience."
