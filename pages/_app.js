60ca9316d04a1044aa5660e585564ef906bf4c23
TeeJayGTAN <oniretemisan@gmail.com>
2023-09-27 16:14:17 +0100

    fb pixel

--- pages/_app.js
+++ pages/_app.js
@@ -21,11 +21,28 @@ function MyApp({ Component, pageProps }) {
         })
       })
   }, [router.events])
-  
+
   return (
     <div className="overflow-hidden">
       <GoogleAnalytics trackPageViews />
       <Analytics />
+            <>
+                  <script>
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '303640862424815');
+                  fbq('track', 'PageView');
+                  </script>
+                  <noscript><img height="1" width="1" style="display:none"
+                  src="https://www.facebook.com/tr?id=303640862424815&ev=PageView&noscript=1"
+                  /></noscript>
+             </>
       <DefaultSeo
         title="TITTA Agency"
         description="We are committed to offering professional services with current technologies backed by years of experience."
