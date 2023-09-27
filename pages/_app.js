68d23d59a3786573dac836a7161a2fbbf5b81085
TeeJayGTAN <oniretemisan@gmail.com>
2023-09-27 16:08:14 +0100

    fb pixel

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,26 @@ import { DefaultSeo } from "next-seo";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import "../styles/globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+ 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1540797856672473') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+  
   return (
     <div className="overflow-hidden">
       <GoogleAnalytics trackPageViews />
